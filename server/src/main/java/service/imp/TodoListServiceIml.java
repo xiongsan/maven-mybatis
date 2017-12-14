21b6437157a38971d45c4e16677668b3e57f7c81
hairui <laodahairui@qq.com>
2017-12-14 17:41:09 +0800

    添加缓存机制

--- server/src/main/java/service/imp/TodoListServiceIml.java
+++ server/src/main/java/service/imp/TodoListServiceIml.java
@@ -2,10 +2,12 @@ package service.imp;
 
 import bean.TodoList;
 import mapper.TodoListMapper;
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import service.ITodoListService;
-
 import java.util.List;
 import java.util.Map;
 
@@ -17,9 +19,21 @@ public class TodoListServiceIml implements ITodoListService {
 
     @Autowired
     private TodoListMapper mapper;
+
+    @Autowired
+    private CacheManager cacheManager;
+
     @Override
+    @SuppressWarnings("unchecked")
     public List<TodoList> todoList() {
-        return mapper.getTodoList();
+        Cache cache=cacheManager.getCache("test");
+        if(cache.get("todolist")!=null){
+            return (List<TodoList>)cache.get("todolist").getValue();
+        }
+        List<TodoList> list = mapper.getTodoList();
+        Element element = new Element("todolist",list);
+        cache.put(element);
+        return list;
     }
 
     @Override
