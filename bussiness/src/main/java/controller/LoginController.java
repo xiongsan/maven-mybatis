ae0df939416f2d437c168b9ceedb6a90cb6e8941
hairui <laodahairui@qq.com>
2018-06-20 14:29:19 +0800

    add locked exception

--- bussiness/src/main/java/controller/LoginController.java
+++ bussiness/src/main/java/controller/LoginController.java
@@ -4,10 +4,7 @@ import com.fable.enclosure.bussiness.interfaces.BaseResponse;
 import com.fable.enclosure.bussiness.util.ResultKit;
 import entity.User;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.ExcessiveAttemptsException;
-import org.apache.shiro.authc.IncorrectCredentialsException;
-import org.apache.shiro.authc.UnknownAccountException;
-import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.subject.Subject;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,8 +31,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     @RequestMapping("/toLogin")
-    public BaseResponse toLogin(@RequestBody User user){
-        UsernamePasswordToken token = new UsernamePasswordToken(user.getLoginName(),user.getPassword());
+    public BaseResponse toLogin(@RequestBody User user) {
+        UsernamePasswordToken token = new UsernamePasswordToken(user.getLoginName(), user.getPassword());
         Subject subject = SecurityUtils.getSubject();
         try {
             subject.login(token);
@@ -48,6 +45,8 @@ public class LoginController {
         } catch (ExcessiveAttemptsException eae) {
             // 捕获错误登录过多的异常
             return ResultKit.fail("fail times a lot error !");
+        } catch (LockedAccountException lae) {
+            return ResultKit.fail("account was locked !");
         }
         subject.getSession().setAttribute("user", user);
         return ResultKit.success();
